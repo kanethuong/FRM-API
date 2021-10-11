4092875c6074c84b4c3d97c735aff5957778e21f
duykhang28 <duykhang28@gmail.com>
2021-10-11 11:45:00 +0700

    Get/Insert/Update/Delete [CompanyService]

--- Services/Company/CompanyService.cs
+++ Services/Company/CompanyService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using kroniiapi.DB;
 using kroniiapi.DB.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace kroniiapi.Services
 {
@@ -16,34 +17,103 @@ namespace kroniiapi.Services
             _dataContext = dataContext;
         }
 
+        /// <summary>
+        /// Get a company by its ID
+        /// </summary>
+        /// <param name="id">ID of company</param>
+        /// <returns>Company data</returns>
         public async Task<Company> GetCompanyById(int id)
         {
-            return null;
+            return await _dataContext.Companies.Where(c => c.CompanyId == id).FirstOrDefaultAsync();
         }
 
+        /// <summary>
+        /// Get a company by its username
+        /// </summary>
+        /// <param name="username">Username of company</param>
+        /// <returns>Company data</returns>
         public async Task<Company> GetCompanyByUsername(string username)
         {
-            return null;
+            return await _dataContext.Companies.Where(c => c.Username.Equals(username)).FirstOrDefaultAsync();
         }
 
+        /// <summary>
+        /// Get a company by its email
+        /// </summary>
+        /// <param name="username">Email of company</param>
+        /// <returns>Company data</returns>
         public async Task<Company> GetCompanyByEmail(string email)
         {
-            return null;
+            return await _dataContext.Companies.Where(c => c.Email.Equals(email)).FirstOrDefaultAsync();
         }
 
+        /// <summary>
+        /// Insert new company to database
+        /// </summary>
+        /// <param name="company">Company data</param>
+        /// <returns>-1: existed / 0: fail / 1: done</returns>
         public async Task<int> InsertNewCompany(Company company)
         {
-            return 0;
+            if (_dataContext.Companies.Any(c => c.Username == company.Username && c.Email == company.Email))
+            {
+                return -1;
+            }
+
+            int rowInserted = 0;
+
+            _dataContext.Companies.Add(company);
+            rowInserted = await _dataContext.SaveChangesAsync();
+
+            return rowInserted;
         }
 
+        /// <summary>
+        /// Update a company with new input data
+        /// </summary>
+        /// <param name="id">ID of company to update</param>
+        /// <param name="company">New company data</param>
+        /// <returns>-1: not found / 0: fail / 1: done</returns>
         public async Task<int> UpdateCompany(int id, Company company)
         {
-            return 0;
+            var existedCompany = await _dataContext.Companies.Where(c => c.CompanyId == id).FirstOrDefaultAsync();
+
+            if (existedCompany == null)
+            {
+                return -1;
+            }
+
+            existedCompany.Fullname = company.Fullname;
+            existedCompany.AvatarURL = company.AvatarURL;
+            existedCompany.Phone = company.Phone;
+            existedCompany.Address = company.Address;
+            existedCompany.Gender = company.Gender;
+            existedCompany.CreatedAt = company.CreatedAt;
+
+            int rowUpdated = 0;
+            rowUpdated = await _dataContext.SaveChangesAsync();
+            return rowUpdated;
         }
 
+        /// <summary>
+        /// Delete a company in database
+        /// </summary>
+        /// <param name="id">ID of company to delete</param>
+        /// <returns>-1: not found / 0: fail / 1: done</returns>
         public async Task<int> DeleteCompany(int id)
         {
-            return 0;
+            var existedCompany = await _dataContext.Companies.Where(c => c.CompanyId == id).FirstOrDefaultAsync();
+
+            if (existedCompany == null)
+            {
+                return -1;
+            }
+
+            existedCompany.IsDeactivated=true;
+            existedCompany.DeactivatedAt=DateTime.Now;
+
+            int rowDeleted = 0;
+            rowDeleted = await _dataContext.SaveChangesAsync();
+            return rowDeleted;
         }
     }
 }
