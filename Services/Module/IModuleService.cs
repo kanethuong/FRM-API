b753e0e8501c057e7000ba7fac0173711820c740
khanhtoanioe <80748497+khanhtoanioe@users.noreply.github.com>
2021-10-12 10:51:36 +0700

    Update [ModuleService], Function in [IModuleServices]

--- Services/Module/IModuleService.cs
+++ Services/Module/IModuleService.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using kroniiapi.DB.Models;
 
 namespace kroniiapi.Services
 {
     public interface IModuleService
     {
-        
+        Task<Module> GetModuleById(int id);
+        Task<int> InsertNewModule(Module module);
+        Task<int> UpdateModule(int id, Module module);
     }
 }
