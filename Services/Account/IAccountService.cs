b2798bd87e87ac78f3f70741a9a967e00088e4f0
anh tho tieu <tieuanhtho@gmail.com>
2021-10-14 01:49:39 +0000

    update InsertNewAccount add function SaveChange [AccountService]

--- Services/Account/IAccountService.cs
+++ Services/Account/IAccountService.cs
@@ -16,5 +16,6 @@ namespace kroniiapi.Services
         Task<int> InsertNewAccount(AccountInput accountInput);
         Task<Tuple<int, IEnumerable<AccountResponse>>> GetDeactivatedAccountList(PaginationParameter paginationParameter);
         Task<int> UpdateAccountPassword(string email, string password);
+        Task<int> SaveChange();
     }
 }
