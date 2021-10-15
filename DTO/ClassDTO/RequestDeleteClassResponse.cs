2f7c8cefd9e23f39bdd8494279e8b889d4cc3a02
huangdaniel0702 <80301492+huangdaniel0702@users.noreply.github.com>
2021-10-15 19:26:35 +0700

    GetRequestDeleteClassList function[Class Controller]

---
+++ DTO/ClassDTO/RequestDeleteClassResponse.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using kroniiapi.DB.Models;
+
+namespace kroniiapi.DTO.ClassDTO
+{
+    public class RequestDeleteClassResponse
+    {
+        public int DeleteClassRequestId { get; set; }
+        public string ClassName { get; set; }
+        public CreatorDTO Admin { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Reason { get; set; }
+    }
+}
