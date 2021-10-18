7e644aad85ef1c2a228f1e498d3d2ecc065c2dc9
thanhdanh2401 <lephamthanhdanh@gmail.com>
2021-10-18 11:38:34 +0700

    Get/Insert/Update[ExamService]

--- Services/Exam/ExamService.cs
+++ Services/Exam/ExamService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using kroniiapi.DB;
 using kroniiapi.DB.Models;
 using kroniiapi.DTO.PaginationDTO;
+using Microsoft.EntityFrameworkCore;
 
 namespace kroniiapi.Services
 {
@@ -15,18 +16,75 @@ namespace kroniiapi.Services
         {
             _dataContext = dataContext;
         }
+        /// <summary>
+        /// Get exam method by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Exam data</returns>
         public async Task<Exam> GetExamById(int id)
         {
-            return null;
+            return await _dataContext.Exams.Where(e => e.ExamId == id).FirstOrDefaultAsync();
         }
+
+        /// <summary>
+        /// Insert new exam method
+        /// </summary>
+        /// <param name="exam"></param>
+        /// <returns>-1:existed / 0:fail / 1:success</returns>
         public async Task<int> InsertNewExam(Exam exam){
-            return 0;
+            if(_dataContext.Exams.Any(e =>
+                e.ExamId == exam.ExamId
+            ))
+            {
+                return -1;
+            }
+            int rowInserted = 0;
+
+            _dataContext.Exams.Add(exam);
+
+            rowInserted = await _dataContext.SaveChangesAsync();
+
+            return rowInserted;
         }
+        
+        /// <summary>
+        /// Update exam method
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="exam"></param>
+        /// <returns>-1:none existed / 0:fail / 1:success</returns>
         public async Task<int> UpdateExam(int id, Exam exam){
-            return 0;
+            var existedExam = await _dataContext.Exams.Where(t => t.ExamId == id).FirstOrDefaultAsync();
+            if(existedExam == null)
+            {
+                return -1;
+            }
+            existedExam.ExamName = exam.ExamName;
+            existedExam.Description = exam.Description;
+            existedExam.ExamDay = exam.ExamDay;
+            existedExam.DurationInMinute = exam.DurationInMinute;
+
+            var rowUpdated = await _dataContext.SaveChangesAsync();
+
+            return rowUpdated;
+            
         }
+
+        /// <summary>
+        /// Pagination Get Exam List method
+        /// </summary>
+        /// <param name="paginationParameter"></param>
+        /// <returns>Exam list</returns>
         public async Task<Tuple<int, IEnumerable<Exam>>> GetExamList(PaginationParameter paginationParameter){
-            return null;
+             var listExam = await _dataContext.Exams.Where(e => e.ExamName.ToUpper().Contains(paginationParameter.SearchName.ToUpper())).ToListAsync();
+
+            int totalRecords = listExam.Count();
+
+            var rs = listExam.OrderBy(e => e.ExamId)
+                     .Skip((paginationParameter.PageNumber - 1) * paginationParameter.PageSize)
+                     .Take(paginationParameter.PageSize);
+
+            return Tuple.Create(totalRecords, rs);
         }
     }
 }
