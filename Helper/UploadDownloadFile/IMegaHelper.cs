af7dbcb0ce3103a7cf4409b3da60ffd362b52eec
duykhang28 <duykhang28@gmail.com>
2021-10-20 21:18:05 +0700

    Add upload image helper and modify UploadDownloadFile folder [UploadDownloadFile(Helper)]

---
+++ Helper/UploadDownloadFile/IMegaHelper.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kroniiapi.Helper.Upload
+{
+    public interface IMegaHelper
+    {
+        Task<Uri> Upload(Stream data, string fileName, string folderName);
+    }
+}
