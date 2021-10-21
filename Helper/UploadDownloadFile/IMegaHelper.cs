2e6f99e6e54d6ba7ce725df43797c9a6d58a6e18
duykhang28 <duykhang28@gmail.com>
2021-10-21 17:39:56 +0700

    Update request delete class [Class Service/ Class Controller]

--- Helper/UploadDownloadFile/IMegaHelper.cs
+++ Helper/UploadDownloadFile/IMegaHelper.cs
@@ -9,9 +9,6 @@ namespace kroniiapi.Helper.Upload
     public interface IMegaHelper
     {
         Task<Uri> Upload(Stream data, string fileName, string folderName);
-<<<<<<< HEAD
-=======
         Task<Stream> Download(Uri uri);
->>>>>>> caa6815fd03229c6ce59ff85216f2fff7fba472d
     }
 }
