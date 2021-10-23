392b54286d364a7192ba20653a3e60e05d90a358
Trần Thiện Phú <hostcode0301@gmail.com>
2021-10-23 17:28:11 +0700

    Fix CORS config for front-end url pro-dev [Startup]

--- Startup.cs
+++ Startup.cs
@@ -108,9 +108,10 @@ namespace kroniiapi
 
             // Access restrict with IAuthorizationHandler
             services.AddSingleton<IAuthorizationHandler, AccessHandler>();
-            
+
             // Config username and password Mega using
-            services.AddSingleton<IMegaHelper>(provider => {
+            services.AddSingleton<IMegaHelper>(provider =>
+            {
                 string username = Configuration["Email:MailAddress"];
                 string password = Configuration["Email:MegaPassword"];
                 return new MegaHelper(username, password);
@@ -192,17 +193,11 @@ namespace kroniiapi
             //Get front-end url from appsettings.json
             var frontEndDevUrl = Configuration["FrontEndDevUrl"];
 
-            //CORS config for Front-end development url
-            app.UseCors(options => options.WithOrigins(frontEndDevUrl)
-                                        .AllowAnyMethod()
-                                        .AllowAnyHeader()
-                                        .AllowCredentials());
-
             //Get front-end url from appsettings.json
             var frontEndUrl = Configuration["FrontEndUrl"];
 
             //CORS config for Front-end url
-            app.UseCors(options => options.WithOrigins(frontEndUrl)
+            app.UseCors(options => options.WithOrigins(frontEndDevUrl, frontEndUrl)
                                         .AllowAnyMethod()
                                         .AllowAnyHeader()
                                         .AllowCredentials());
