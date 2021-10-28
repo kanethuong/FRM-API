522b246dedd28c9c0e6c4c1bca7a23f6c409b7ad
Toàn Trần Khánh <trankhanhtoan14052001@gmail.com>
2021-10-28 05:01:01 +0000

    add orderby and searchname [Trainee,trainer, module service]

--- Services/Trainee/TraineeService.cs
+++ Services/Trainee/TraineeService.cs
@@ -388,7 +388,12 @@ namespace kroniiapi.Services
         public async Task<Tuple<int, IEnumerable<Trainee>>> GetAllTraineeWithoutClass(PaginationParameter paginationParameter)
         {
             var traineeList = await _dataContext.Trainees.Where(t
-                 => t.IsDeactivated == false && t.ClassId == null && t.Email.ToUpper().Contains(paginationParameter.SearchName.ToUpper())).ToListAsync();
+                 => t.IsDeactivated == false && t.ClassId == null && 
+                                                (t.Email.ToUpper().Contains(paginationParameter.SearchName.ToUpper()) ||
+                                                t.Username.ToUpper().Contains(paginationParameter.SearchName.ToUpper())  ||
+                                                t.Fullname.ToUpper().Contains(paginationParameter.SearchName.ToUpper())))
+                                                .OrderByDescending(t=>t.CreatedAt)
+                                                .ToListAsync();
             return Tuple.Create(traineeList.Count(), PaginationHelper.GetPage(traineeList,
                 paginationParameter.PageSize, paginationParameter.PageNumber));
         }
