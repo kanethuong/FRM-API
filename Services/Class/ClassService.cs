24593fd25e69391ae20f7f37c3c285deacc06aeb
Nhật Hoàng <alberthheinz@gmail.com>
2021-10-31 12:22:43 +0000

    Fix: big bug ViewClassFeedback, bug ViewClassScore, bug SubmitCertificate

--- Services/Class/ClassService.cs
+++ Services/Class/ClassService.cs
@@ -240,7 +240,6 @@ namespace kroniiapi.Services
         /// <returns>tuple list of trainee</returns>
         public async Task<Tuple<int, IEnumerable<Trainee>>> GetTraineesByClassId(int id, PaginationParameter paginationParameter)
         {
-
             var traineeList = await _dataContext.Trainees.Where(t => t.ClassId == id && t.Fullname.ToUpper().Contains(paginationParameter.SearchName.ToUpper())).ToListAsync();
             int totalRecords = traineeList.Count();
             var rs = traineeList.OrderBy(c => c.TraineeId)
