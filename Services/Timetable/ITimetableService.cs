4da85bbe60d4061f748d40ae969400f2390b5e2d
Nguyễn Thịnh <sakakiyuya1412@gmail.com>
2021-11-01 01:54:58 +0000

    TimetableAltho [TimetableHelper/TimetableService/TimetableController]

---
+++ Services/Timetable/ITimetableService.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using kroniiapi.DB.Models;
+
+namespace kroniiapi.Services
+{
+    public interface ITimetableService
+    {
+        Task<int> InsertModuleToClass(int moduleId, int classId,  int numSlotWeek);
+        bool CheckAvailableModule(ICollection<Module> modulesList, DateTime startDay, DateTime endDay);
+        Task<ICollection<Module>> GetModuleListlByClassId(int classId);
+        int CalculateSlotForWeek(int moduleSlot, DateTime startDay, DateTime endDay);
+        bool CheckAvailabeSlotsForRoom(int slotsNeed, int roomId, DateTime startDay, DateTime endDay);
+        Task<IEnumerable<string>> GetOtherRoomsForClass(int slotsNeed, int roomId, DateTime startDay, DateTime endDay);
+        int GetTotalSlotsNeed(ICollection<Module> modulesList);
+        bool CheckAvailabeSlotsForTrainer(int slotsNeed, int trainerId, DateTime startDay, DateTime endDay);
+        void SaveChangeTimeTable();
+    }
+}
