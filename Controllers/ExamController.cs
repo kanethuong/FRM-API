21a3e42d87636d6e9c8887bd339f9cd6a6a5283d
Trần Thiện Phú <hostcode0301@gmail.com>
2021-11-01 03:58:50 +0000

    View exam list, update exam info, cancel exam [ExamController, ExamService, wwwroot]

--- Controllers/ExamController.cs
+++ Controllers/ExamController.cs
@@ -2,8 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using kroniiapi.DB.Models;
+using kroniiapi.DTO;
 using kroniiapi.DTO.ExamDTO;
 using kroniiapi.DTO.PaginationDTO;
+using kroniiapi.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace kroniiapi.Controllers
@@ -12,10 +16,15 @@ namespace kroniiapi.Controllers
     [Route("api/[controller]")]
     public class ExamController : ControllerBase
     {
-        public ExamController()
+        private readonly IExamService _examService;
+        private readonly IMapper _mapper;
+
+        public ExamController(IExamService examService, IMapper mapper)
         {
-            
+            _examService = examService;
+            _mapper = mapper;
         }
+
         /// <summary>
         /// create new exam 
         /// </summary>
@@ -29,13 +38,29 @@ namespace kroniiapi.Controllers
         }
         /// <summary>
         /// View all exam with pagination
-        /// </summary>
-        /// <returns></returns>
+        /// /// </summary>
+        /// <returns>200: All exams list / 404: Exam name not found</returns>
         [HttpGet("page")]
         public async Task<ActionResult<PaginationResponse<IEnumerable<ExamResponse>>>> ViewExamList([FromQuery] PaginationParameter paginationParameter)
         {
-            return null;
+            var tuple = await _examService.GetExamList(paginationParameter);
+            var totalRecord = tuple.Item1;
+            var examList = tuple.Item2;
+
+            if (totalRecord == 0)
+            {
+                return NotFound(new ResponseDTO(404, "Exam not found"));
+            }
+
+            var rs = new PaginationResponse<ICollection<ExamResponse>>()
+            {
+                TotalRecords = totalRecord,
+                Payload = _mapper.Map<ICollection<ExamResponse>>(examList)
+            };
+
+            return Ok(rs);
         }
+
         /// <summary>
         /// Change Exam infomation, only duration and exam day
         /// </summary>
@@ -44,19 +69,54 @@ namespace kroniiapi.Controllers
         /// <param name="ExamDay"></param>
         /// <returns></returns>
         [HttpPut("{id:int}")]
-        public async Task<ActionResult> ChangeExamInfo(int id, int duration, DateTime ExamDay)
+        public async Task<ActionResult> ChangeExamInfo(int id, [FromBody] UpdateExamInput updateExamInput)
         {
-            return null;
+            var exam = await _examService.GetExamById(id);
+
+            if (exam == null)
+            {
+                return NotFound(new ResponseDTO(404, "Exam not found"));
+            }
+
+            exam.DurationInMinute = updateExamInput.Duration;
+            exam.ExamDay = updateExamInput.ExamDay;
+
+            var rs = await _examService.UpdateExam(id, exam);
+
+            if (rs == -1)
+            {
+                return BadRequest(new ResponseDTO(400, "Exam was cancelled"));
+            }
+
+            if (rs == 0)
+            {
+                return BadRequest(new ResponseDTO(400, "There is an error when update exam"));
+            }
+
+            return Ok(new ResponseDTO(200, "Update exam successfully"));
         }
+
         /// <summary>
         /// Cancel an exam
         /// </summary>
         /// <param name="id">id of exam</param>
-        /// <returns></returns>
-        [HttpPut("cancel/{id:int}")]
+        /// <returns>200: Cancelled / 404: Id not found, 409: Exam was cancelled, Delete fail</returns>
+        [HttpDelete("{id:int}")]
         public async Task<ActionResult> CancelExam(int id)
         {
-            return null;
+            (bool? status, string message) = await _examService.CancelExam(id);
+
+            if (status == null)
+            {
+                return NotFound(new ResponseDTO(404, message));
+            }
+
+            if (status == false)
+            {
+                return Conflict(new ResponseDTO(409, message));
+            }
+
+            return Ok(new ResponseDTO(200, message));
         }
     }
 }
