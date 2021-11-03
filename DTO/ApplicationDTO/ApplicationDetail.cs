53326ec952ef2b4eef4b82edf24b2acb2b8503f9
unknown <lephamthanhdanh@gmail.com>
2021-11-03 16:55:01 +0700

    Add IsAccepted field [ApplicationDetail] update doc response body [wwwroot]

--- DTO/ApplicationDTO/ApplicationDetail.cs
+++ DTO/ApplicationDTO/ApplicationDetail.cs
@@ -14,5 +14,6 @@ namespace kroniiapi.DTO.ApplicationDTO
         public string Description { get; set; }
         public string ApplicationURL { get; set; }
         public string Response { get; set; }
+        public bool IsAccepted { get; set; }
     }
 }
