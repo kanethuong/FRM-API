f0f18ec30ebac58065f3092f5dd0b1c4bff19117
unknown <lephamthanhdanh@gmail.com>
2021-11-05 14:29:04 +0700

    Update AssignModuleInput [DTO]

--- DTO/ClassDTO/AssignModuleInput.cs
+++ DTO/ClassDTO/AssignModuleInput.cs
@@ -11,5 +11,7 @@ namespace kroniiapi.DTO.ClassDTO
         [Required]
         public int ClassId { get; set; }
         public int ModuleId { get; set; }
+        public int TrainerId { get; set; }
+
     }
 }
