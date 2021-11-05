dfdc3af2197f5e8310e92488c55a80de7f4612c1
Trần Thiện Phú <hostcode0301@gmail.com>
2021-11-05 13:01:48 +0000

    Fulltext search when get all [ExamService]

---
+++ Migrations/20211105123745_fullTextSearch.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace kroniiapi.Migrations
+{
+    public partial class fullTextSearch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Exams_ExamName",
+                table: "Exams",
+                column: "ExamName")
+                .Annotation("Npgsql:TsVectorConfig", "simple");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Exams_ExamName",
+                table: "Exams");
+        }
+    }
+}
