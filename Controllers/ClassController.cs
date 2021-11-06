67b4f5b45ef578c1bc7145e93f8308d74b84443a
anh tho tieu <tieuanhtho@gmail.com>
2021-11-06 03:39:53 +0000

    add function RemoveModule [classControleer]

--- Controllers/ClassController.cs
+++ Controllers/ClassController.cs
@@ -33,13 +33,19 @@ namespace kroniiapi.Controllers
         private readonly ITraineeService _traineeService;
         private readonly ITimetableService _timetableService;
 
+        private readonly IMarkService _markService;
+
+        private readonly ICertificateService _certificateService;
+
         public ClassController(IClassService classService,
                                ITraineeService traineeService,
                                IAdminService adminService,
                                IModuleService moduleService,
                                ITrainerService trainerService,
                                IMapper mapper,
-                               ITimetableService timetableService)
+                               ITimetableService timetableService,
+                               IMarkService markService,
+                               ICertificateService certificateService)
         {
             _classService = classService;
             _adminService = adminService;
@@ -49,6 +55,8 @@ namespace kroniiapi.Controllers
             _timetableService = timetableService;
             _adminService = adminService;
             _traineeService = traineeService;
+            _markService = markService;
+            _certificateService = certificateService;
         }
 
         /// <summary>
@@ -287,7 +295,7 @@ namespace kroniiapi.Controllers
             if (result != 1)
             {
                 return BadRequest(new ResponseDTO(400, message));
-            } 
+            }
             else
                 return Created("", new ResponseDTO(201, "Successfully inserted class with timetable"));
         }
@@ -621,8 +629,40 @@ namespace kroniiapi.Controllers
         [HttpDelete("module/{moduleId:int}")]
         public async Task<ActionResult> RemoveModule(AssignModuleInput assignModuleInput)
         {
-            return null;
-        }
+            var classInfor = await _classService.GetClassByClassID(assignModuleInput.ClassId);
+            if(classInfor == null)
+            {
+                return NotFound(new ResponseDTO(404, "Class is not exist"));
+            }
+            if(classInfor.TrainerId != assignModuleInput.TrainerId)
+            {
+                return BadRequest(new ResponseDTO(409, "Trainer are not in this class"));
+            }
+            var moduleMarkState = await _markService.GetMarkByModuleId(assignModuleInput.ModuleId, null, null);
+            if (moduleMarkState.Count() != 0)
+            {
+                return NotFound(new ResponseDTO(404, "Trainee in this class has score with this module"));
+            }
 
+            var moduleCertificateState = await _certificateService.GetCertificatesURLByModuleId(assignModuleInput.ModuleId);
+            if (moduleCertificateState.Count() != 0)
+            {
+                return NotFound(new ResponseDTO(404, "Trainee in this class has certificate with this module"));
+            }
+
+            int removeStatus = await _classService.RemoveModuleFromClass(assignModuleInput.ClassId, assignModuleInput.ModuleId);
+            if(removeStatus == -1)
+            {
+                return BadRequest(new ResponseDTO(409, "Class does not have this module"));
+            }
+            else if(removeStatus == 1)
+            {
+                return Ok(new ResponseDTO(200, "Deleted!"));
+            }
+            else
+            {
+                return BadRequest(new ResponseDTO(409, "Fail to delete! Unexpected error"));
+            }
+        }
     }
 }
