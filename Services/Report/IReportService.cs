5dac57fc4825f796122b1a834b6280718dd890ee
Lê Phạm Thành Danh <lephamthanhdanh@gmail.com>
2021-11-10 06:11:04 +0000

    GetTraineesInfo - GetClassStatusReport [ReportService]

--- Services/Report/IReportService.cs
+++ Services/Report/IReportService.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using kroniiapi.DTO.ReportDTO;
 
 namespace kroniiapi.Services.Report
 {
     public interface IReportService
     {
-        
+        ICollection<TraineeGeneralInfo> GetTraineesInfo(int classId);
+        ClassStatusReport GetClassStatusReport(int classId);
     }
 }
