9fc7f025909bbfbc021c962ce9fb3c8824c363a1
Thịnh Nguyễn <sakakiyuya1412@gmail.com>
2021-11-11 15:23:11 +0700

    update ModuleExcelInput

--- DTO/TimetableDTO/ModuleExcelInput.cs
+++ DTO/TimetableDTO/ModuleExcelInput.cs
@@ -8,6 +8,10 @@ namespace kroniiapi.DTO.TimetableDTO
     public class ModuleExcelInput
     {
         public int Day { set; get; }
+        public IEnumerable<DayContent> DayContents { set; get; }
+    }
+    public class DayContent
+    {
         public string Lecture { set; get; }
         public string DeliveryType { set; get; }
         public int Duration_mins { set; get; }
