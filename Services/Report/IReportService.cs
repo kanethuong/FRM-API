788f29b70ca57ea09cd24eddd548383554802bff
Toàn Trần Khánh <trankhanhtoan14052001@gmail.com>
2021-11-15 08:38:44 +0000

    get module by id [module controller]

--- Services/Report/IReportService.cs
+++ Services/Report/IReportService.cs
@@ -18,7 +18,7 @@ namespace kroniiapi.Services.Report
         ICollection<RewardAndPenalty> GetRewardAndPenaltyScore(int classId, DateTime reportAt = default(DateTime));
         ICollection<TraineeFeedback> GetTraineeFeedbacks(int classId, DateTime reportAt = default(DateTime));
         List<FeedbackReport> GetFeedbackReport(int classId, DateTime reportAt = default(DateTime));
-        Dictionary<int, List<AttendanceReport>> GetAttendanceReportEachMonth(int classId, int monthReport);
+        Dictionary<DateTime, List<AttendanceReport>> GetAttendanceReportEachMonth(int classId, DateTime monthReport);
         List<AttendanceReport> GetTotalAttendanceReports(int classId);
         Task<CheckpointReport> GetCheckpointReport(int classId);
     }
