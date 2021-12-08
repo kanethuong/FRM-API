3cf4af961f10e29a1ad57f79b4bedf5788481fd2
Trần Thiện Phú <hostcode0301@gmail.com>
2021-12-08 02:02:11 +0000

    Catch exception when refresh token [TokenController]

--- Controllers/TokenController.cs
+++ Controllers/TokenController.cs
@@ -44,43 +44,50 @@ namespace kroniiapi.Controllers
         [HttpPost("refresh")]
         public async Task<ActionResult<Token>> RefreshAccessToken()
         {
-            // Get bearer token from Header
-            var _bearer_token = Request.Headers[HeaderNames.Authorization].ToString().Replace("Bearer ", "");
+            try
+            {
+                // Get bearer token from Header
+                var _bearer_token = Request.Headers[HeaderNames.Authorization].ToString().Replace("Bearer ", "");
 
-            // Get payload data in access token
-            var principal = _jwtGenerator.GetPrincipalFromExpiredToken(_bearer_token);
+                // Get payload data in access token
+                var principal = _jwtGenerator.GetPrincipalFromExpiredToken(_bearer_token);
 
-            // Get email, role claim in user access token (payload)
-            var claims = principal.Identities.First().Claims.ToList();
-            var email = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
-            var role = claims?.FirstOrDefault(c => c.Type.EndsWith("role", StringComparison.CurrentCultureIgnoreCase))?.Value;
+                // Get email, role claim in user access token (payload)
+                var claims = principal.Identities.First().Claims.ToList();
+                var email = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+                var role = claims?.FirstOrDefault(c => c.Type.EndsWith("role", StringComparison.CurrentCultureIgnoreCase))?.Value;
 
-            // Check existed email in db with that role
-            var account = await _accountService.GetAccountByEmail(email);
+                // Check existed email in db with that role
+                var account = await _accountService.GetAccountByEmail(email);
 
-            if (account == null || !account.Item1.Role.Equals(role, StringComparison.CurrentCultureIgnoreCase))
+                if (account == null || !account.Item1.Role.Equals(role, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    return BadRequest(new ResponseDTO(400, "Invalid access token"));
+                }
+
+                // Get refresh token from cookies
+                if (!Request.Cookies.TryGetValue("X-Refresh-Token", out var refreshToken))
+                {
+                    return BadRequest(new ResponseDTO(400, "Invalid token request"));
+                }
+
+                // Try to check expired and get email from refresh token
+                var tokenMail = _refreshToken.GetEmailByToken(refreshToken); // Included expired check
+
+                if (tokenMail == null || tokenMail != refreshToken)
+                {
+                    return BadRequest(new ResponseDTO(400, "Invalid client request"));
+                }
+
+                // Generate new tokens
+                var newAccessToken = _jwtGenerator.GenerateAccessToken(principal.Claims);
+
+                return Ok(new Token { AccessToken = newAccessToken });
+            }
+            catch
             {
                 return BadRequest(new ResponseDTO(400, "Invalid access token"));
             }
-
-            // Get refresh token from cookies
-            if (!Request.Cookies.TryGetValue("X-Refresh-Token", out var refreshToken))
-            {
-                return BadRequest(new ResponseDTO(400, "Invalid token request"));
-            }
-
-            // Try to check expired and get email from refresh token
-            var tokenMail = _refreshToken.GetEmailByToken(refreshToken); // Included expired check
-
-            if (tokenMail == null || tokenMail != refreshToken)
-            {
-                return BadRequest(new ResponseDTO(400, "Invalid client request"));
-            }
-
-            // Generate new tokens
-            var newAccessToken = _jwtGenerator.GenerateAccessToken(principal.Claims);
-
-            return Ok(new Token { AccessToken = newAccessToken });
         }
 
         /// <summary>
